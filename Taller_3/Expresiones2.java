3f901181fc2839c3ff6036e6046e73119305e964
aeencalada <aeencalada@gmail.com>
2020-11-06 03:11:48 -0500

    Taller 3

---
+++ Taller_3/Expresiones2.java
@@ -0,0 +1,38 @@
+public class Expresiones2 {
+	public static void main (String[] args){
+		float resp1;
+		float resp2;
+		byte a = 100;
+		byte b = -12;
+		short c = 4000;
+		byte d = 5;
+		byte e = 0;
+		boolean resp6;
+
+		System.out.println("EVALUACIÓN DE EXPRESIONES");
+		System.out.println("=========================\n");
+
+		resp1 = 2 * (float) 8 / 2 / 2 - 22 * (float) 4 / 8 - ( 100 + 25 ) / (float) Math.pow(5,2);
+
+		System.out.println("Expresión 1:");
+		System.out.println("    2 * 8 / 2 / 2 – 22 * 4 / 8 - (100 + 25) / 5 ^ 2");
+		System.out.println("Resultado:");
+		System.out.println("    " + resp1);
+
+		resp2 = (float) ( ( 2 * 16.0 / Math.pow(2,3) - 4 ) + 32 - ( 125.0 / 5 / 5 + 3 ) );
+
+		System.out.println("\nExpresión 2:");
+		System.out.println("    ( 2 * 16 / 2 ^ 3 – 4 ) + 32 - ( 125 / 5 / 5 + 3 )");
+		System.out.println("Resultado:");
+		System.out.println("    " + resp2);
+
+		resp6 = ( a + b * c + e ) <= e * d * b && ( (float) c / a + e - d ) < e + d - c + b * a;
+
+		System.out.println("\nExpresión 6:");
+		System.out.println("    ( a + b * c + e ) <= e * d * b AND ( c / a + e - d) < e + d – c + b * a");
+		System.out.println("    donde: a = 100 , b = -12 , c = 4000 , d = 5 , e = 0");
+		System.out.println("Resultado:");
+		System.out.println("    " + resp6);
+
+	}
+}
---
+++ Taller_3/Expresiones3.java
@@ -0,0 +1,27 @@
+public class Expresiones3 {
+	public static void main (String[] args){
+		// Declaración de variables
+		float tam_fotos_hd_GB;
+		float tam_fotos_sd_GB;
+		float tam_videos_GB;
+		float total_MS;		// Cantidad de Memory Sticks
+
+		// Cálculos Problema 3
+		tam_fotos_hd_GB = (float) 500 * 64 / 1024;
+		tam_fotos_sd_GB = (float) 2000 * 8300 / 1024 / 1024;
+		tam_videos_GB = 50 * 4;
+		total_MS = ( tam_fotos_hd_GB + tam_fotos_sd_GB + tam_videos_GB ) / 32;
+
+		// SALIDAS
+		System.out.println("UNIDADES DE ALMACENAMIENTO");
+		System.out.println("==========================");
+		System.out.println("\nProblema 3:");
+		System.out.print("Un fotógrafo utiliza una cámara de alta definición para capturar fotos ");
+		System.out.print("panorámicas en excursiones a reservas naturales. Para almacenar las ");
+		System.out.println("fotos lleva Memory Sticks de 32GB cada una. ¿Cuántos Memory Stick debe " + 
+						 "llevar para almacenar 500 fotos de alta definición de 64MB cada una, 2000 " + 
+						 "fotos de mediana definición de 8300KB cada una y 50 videos de 4GB cada uno?\n");
+		System.out.println("El fotógrafo debe llevar " + total_MS + " Memory Sticks");
+
+	}
+}
